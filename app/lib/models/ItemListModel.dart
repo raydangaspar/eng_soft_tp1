179ba746446cdb86e7d0b5ffc5bb284bd7456db6
Matheus Vargas <m6.vargas@gmail.com>
2019-10-07 22:50:17 -0300

    Adição do model para item do estoque

---
+++ app/lib/models/ItemListModel.dart
@@ -0,0 +1,31 @@
+class ItemList{
+  List<ItemDetailListItem> itemList;
+
+  ItemList({this.itemList});
+
+  factory ItemList.fromJSON(Map<dynamic,dynamic> json){
+    return ItemList(
+        itemList: parseitens(json)
+    );
+  }
+
+  static List<ItemDetailListItem> parseitens(itemJSON){
+    var iList=itemJSON['browseItens'] as List;
+    List<ItemDetailListItem> recipeList=iList.map((data) => ItemDetailListItem.fromJson(data)).toList();
+    return recipeList;
+  }
+}
+
+
+class ItemDetailListItem {
+  String itemTitle;
+  String iconUrl;
+  String value;
+  String uid;
+
+  ItemDetailListItem({this.itemTitle,this.iconUrl,this.value,this.uid});
+
+  factory ItemDetailListItem.fromJson(Map<dynamic,dynamic> parsedJson) {
+    return ItemDetailListItem(itemTitle:parsedJson['Name'],iconUrl: parsedJson['icon'],value:parsedJson['value'],uid: parsedJson['uid']);
+  }
+}
