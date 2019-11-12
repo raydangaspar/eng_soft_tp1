9044a60c2ba2b1a720ccfbe5fde6801e9c1e8e12
Gabriel P <gabrielpires@dcc.ufmg.br>
2019-11-11 21:24:49 -0300

    Função de logout

---
+++ app/lib/catalog_screen.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:app/cart.dart';
+import 'package:app/catalog.dart';
+
+import 'package:app/auth.dart';
+
+class MyCatalog extends StatelessWidget {
+  MyCatalog({this.auth, this.onSignedOut});
+  final BaseAuth auth;
+  final VoidCallback onSignedOut;
+
+  void _signOut() async {
+    try{
+      await auth.signOut();
+      onSignedOut();
+    }
+    catch(e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+        SliverAppBar(
+          title: Text('Catalogo de Produtos', style: Theme.of(context).textTheme.display4),
+          floating: true,
+          actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () => Navigator.pushNamed(context, '/cart'),
+          ),
+          FlatButton(
+              child: Text('Logout', style: TextStyle(fontSize: 17, color: Colors.white)),
+              onPressed: _signOut
+          )
+        ],
+      ),
+          SliverToBoxAdapter(child: SizedBox(height: 12)),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+                (context, index) => _MyListItem(index)),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _AddButton extends StatelessWidget {
+  final Item item;
+
+  const _AddButton({Key key, @required this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var cart = Provider.of<CartModel>(context);
+
+    return FlatButton(
+      onPressed: cart.items.contains(item) ? null : () => cart.add(item),
+      splashColor: Theme.of(context).primaryColor,
+      child: cart.items.contains(item)
+          ? Icon(Icons.check, semanticLabel: 'ADDED')
+          : Text('ADD'),
+    );
+  }
+}
+
+class _RmvButton extends StatelessWidget {
+  final Item item;
+
+  const _RmvButton({Key key, @required this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var cart = Provider.of<CartModel>(context);
+
+    return FlatButton(
+      onPressed: cart.items.contains(item) ? () => cart.rmv(item) : null ,
+      splashColor: Theme.of(context).primaryColor,
+      child: cart.items.contains(item)
+          ? Icon(Icons.remove, semanticLabel: 'RMVED')
+          : Text('RMV'),
+    );
+  }
+}
+
+
+class _Add_Rmv_Button extends StatelessWidget {
+  final Item item;
+
+  const _Add_Rmv_Button({Key key, @required this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var cart = Provider.of<CartModel>(context);
+
+  if (cart.items.contains(item) ) {
+      return FlatButton(
+      onPressed: cart.items.contains(item) ? () => cart.rmv(item) : null ,
+      splashColor: Theme.of(context).primaryColor,
+      child: cart.items.contains(item)
+          ? Icon(Icons.delete_forever, semanticLabel: 'RMVED')
+          : Text('RMV'),
+    );
+  } else {
+      return FlatButton(
+      onPressed: cart.items.contains(item) ? null : () => cart.add(item),
+      splashColor: Theme.of(context).primaryColor,
+      child: cart.items.contains(item)
+          ? Text('ADD')
+          : Icon(Icons.control_point, semanticLabel: 'ADDED'),
+    );
+  }
+
+  }
+}
+
+
+class _MyAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      title: Text('Catalogo de Produtos', style: Theme.of(context).textTheme.display4),
+      floating: true,
+      actions: [
+        IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () => Navigator.pushNamed(context, '/cart'),
+        ),
+      ],
+    );
+  }
+}
+
+class _MyListItem extends StatelessWidget {
+  final int index;
+
+  _MyListItem(this.index, {Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var catalog = Provider.of<CatalogModel>(context);
+    var item = catalog.getByPosition(index);
+    var textTheme = Theme.of(context).textTheme.title;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: LimitedBox(
+        maxHeight: 48,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 1,
+              child: Container(
+                color: item.color,
+              ),
+            ),
+            SizedBox(width: 24),
+            Expanded(
+              child: Text(item.name, style: textTheme),
+            ),
+
+            SizedBox(width: 10),
+            Expanded(
+              child: Text(item.price.toString()),
+            ),
+
+            //SizedBox(),
+            //_AddButton(item: item),
+
+             SizedBox(),
+            _Add_Rmv_Button(item: item,)
+          ],
+        ),
+      ),
+    );
+  }
+}
